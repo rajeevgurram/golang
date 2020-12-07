33c3ecb51054aa2e0766126e43b4b8e54628e9ab
Rajeev Gurram <rajeev.sjsu@gmail.com>
2020-12-06 21:32:51 -0800

    added calculator package

---
+++ educative/lecture1/calculator/calculator.go
@@ -0,0 +1,23 @@
+package calculator
+
+// Sum of 2 numbers
+func Sum(a int, b int) int {
+	return a + b
+}
+
+// Sub 2 numbers
+func Sub(a, b int) int {
+	return a - b
+}
+
+// Mul 2 numbers
+func Mul(a, b int) int {
+	c := a * b
+	return c
+}
+
+// Div 2 numbers
+func Div(a, b int) int {
+	c := a / b
+	return c
+}
--- hello_world.go
+++ hello_world.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"./educative/lecture1/calculator"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	// Calculator
+	newLecture(1, "Calculator")
+	var num1, num2 int = 10, 20
+	fmt.Printf("Sum of %d + %d = %d \n", num1, num2, calculator.Sum(num1, num2))
+	fmt.Printf("Sub of %d - %d = %d \n", num1, num2, calculator.Sub(num1, num2))
+	fmt.Printf("Mul of %d X %d = %d \n", num1, num2, calculator.Mul(num1, num2))
+	fmt.Printf("Div of %d / %d = %d \n", num1, num2, calculator.Div(num2, num1))
+}
+
+func newLecture(lectureNumber int, lectureName string) {
+	fmt.Printf("\n\nLecture %d, %s\n", lectureNumber, lectureName)
+	fmt.Printf("_______________________________\n\n")
 }
